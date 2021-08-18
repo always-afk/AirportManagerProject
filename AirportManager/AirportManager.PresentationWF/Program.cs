09f3567e60f0639a579e6273d2fe8503a5224599
Артём Кармальков <artemkarmalkov@gmail.com>
2021-08-18 12:01:31 +0300

    AddInjector

--- AirportManager/AirportManager.PresentationWF/Program.cs
+++ AirportManager/AirportManager.PresentationWF/Program.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimpleInjector;
 
 namespace AirportManager.PresentationWF
 {
     static class Program
     {
+        static readonly Container _container;
+        static Program()
+        {
+            _container = new Container();
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -17,7 +24,15 @@ namespace AirportManager.PresentationWF
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forms.LoginForm());
+            Register();
+            Application.Run(_container.GetInstance<Forms.LoginForm>());
+        }
+
+        static void Register()
+        {
+            _container.Register<Forms.LoginForm>(Lifestyle.Singleton);
+
+            _container.Verify();
         }
     }
 }
